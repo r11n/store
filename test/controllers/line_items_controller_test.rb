12c0139315d0ae252daea800065402107b639a48
r11n <raghavendra.n@greycampus.com>
2017-06-12 09:54:24 +0530

    adding ajax

--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -21,7 +21,7 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
     end
     follow_redirect!
     assert_select 'h2', 'Your Cart'
-    assert_select 'li', 'Programming Ruby 1.9'
+    assert_select 'td', 'Programming Ruby 1.9'
     #assert_redirected_to line_item_url(LineItem.last)
   end
 
@@ -36,8 +36,9 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update line_item" do
-    patch line_item_url(@line_item), params: { line_item: { cart_id: @line_item.cart_id, product_id: @line_item.product_id } }
-    assert_redirected_to line_item_url(@line_item)
+  patch line_item_url(@line_item),
+    params: { line_item: { product_id: @line_item.product_id } }
+  assert_redirected_to line_item_url(@line_item)
   end
 
   test "should destroy line_item" do
